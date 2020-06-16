162565f11a9969cb51819114fa3fc9ef3d8fd78e
JaeWook Lee <2jae66@gmail.com>
2020-06-16 22:20:06 +0900

    2020년 6월 16일 알고리즘

--- 프로그래머스/소수 찾기/SearchSosu/SearchSosu/main.swift
+++ 프로그래머스/소수 찾기/SearchSosu/SearchSosu/main.swift
@@ -9,9 +9,19 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
+    var count = 0
+    var arr = Array.init(repeating: false, count: n + 1)
 
+    for i in 2...n{
+        if arr[i] == false{
+            count += 1
+            for j in stride(from: i, to: n + 1, by: i){
+                arr[j] = true
+            }
+        }
+    }
 
-
+    return count
 }
 solution(10)
 
