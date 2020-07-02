8e4daddd14bfe95e56eccfaa1013f45f791bda95
JaeWook Lee <2jae66@gmail.com>
2020-07-02 22:27:27 +0900

    2020년 07월 2일

---
+++ 프로그래머스/Level2/위장/wujang/wujang/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  wujang
+//
+//  Created by 1 on 2020/07/02.
+//  Copyright © 2020 wook. All rights reserved.
+//
+
+import Foundation
+
+func solution(_ clothes:[[String]]) -> Int {
+    var kind: [String: Int] = [:]
+    
+    for i in 0 ..< clothes.count{
+        let key = clothes[i][1]
+        let valueCount = kind[key] ?? 1
+        kind[key] = valueCount + 1
+    }
+   
+   // print(kind.reduce(1){$0 * $1.value} - 1)
+    return kind.reduce(1){$0 * $1.value} - 1
+}
+
+solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]])
+
