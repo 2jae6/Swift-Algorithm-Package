70138b031f09f4c497a85e1f3da176d21dfe16cd
JaeWook Lee <2jae66@gmail.com>
2020-09-20 22:06:49 +0900

    dfx Example

---
+++ 알고리즘 공부/dfsEx/dfsEx/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  dfsEx
+//
+//  Created by 1 on 2020/07/07.
+//  Copyright © 2020 wook. All rights reserved.
+//
+
+import Foundation
+//궁극
+var count = 0
+
+
+func dfs(_ n: Int, _ edges: [(Int, Int)]){
+    //멈춤 - 마지막이 n일 때
+    // 경로니까 방문했던 과정이 필요함
+    if n ==
+    
+    
+    
+    
+}
+
+
+func solution(_ n: Int, _ edges: [(Int, Int)]) -> Int {
+    
+    return 0
+}
+solution(5, [(1, 2), (1, 3), (1, 4), (2, 1), (2, 4), (2, 5), (3, 2), (3, 4), (4, 5)])
